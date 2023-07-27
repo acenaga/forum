420ed857574f6db1097b448b751342449248da35
Carlos Ferrer <mundofuturoca@gmail.com>
2023-07-27 16:52:37 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ThreadController;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', \App\Http\Livewire\ShowThreads::class)
+    ->middleware('auth')
+    ->name('dashboard');
+
+Route::get('/thread/{thread}', \App\Http\Livewire\ShowThread::class)
+    ->middleware('auth')
+    ->name('thread');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('threads', ThreadController::class)->except([
+        'show', 'index', 'destroy'
+    ]);
+});
+
+require __DIR__.'/auth.php';
